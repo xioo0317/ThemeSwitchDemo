131c2ae1a60f4583e7c538c3ab88e11c4c29cae9
xioo0317 <2977401730@qq.com>
2026-09-15 15:04:06 +0800

    Fix SettingsMiuix: add Box import and remove dummy function

--- app/src/main/java/com/demo/themeswitch/ui/screen/SettingsMiuix.kt
+++ app/src/main/java/com/demo/themeswitch/ui/screen/SettingsMiuix.kt
@@ -2,6 +2,7 @@ package com.demo.themeswitch.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -247,5 +248,3 @@ private fun MiuixSettingsItem(
         }
     }
 }
-
-private fun Box(modifier: Modifier) {}
