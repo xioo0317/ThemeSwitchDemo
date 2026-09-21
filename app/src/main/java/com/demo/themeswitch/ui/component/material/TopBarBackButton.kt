004028f901a1654c02695a03fa1586c4e80f6e6a
xioo0317 <2977401730@qq.com>
2026-09-22 01:33:13 +0800

    Navigation, entry screens, scaffold locals, TopBarBackButton

--- app/src/main/java/com/demo/themeswitch/ui/component/material/TopBarBackButton.kt
+++ app/src/main/java/com/demo/themeswitch/ui/component/material/TopBarBackButton.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun TopBarBackButton(
