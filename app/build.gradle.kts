4b36e60f8ebbe9851ad92435810965f1d1c72135
xioo0317 <2977401730@qq.com>
2026-09-22 01:19:53 +0800

    Port KernelSU UI: build/manifest/splash themes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-splashscreen:1.2.0")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
